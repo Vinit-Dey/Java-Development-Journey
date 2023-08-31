daf7971f61622d2b684a858f476f624cc683bcac
Vinit Dey <deyvinit9@gmail.com>
2023-08-31 23:50:00 +0530

    using try statement

--- Odd_Even.java
+++ Odd_Even.java
@@ -1,15 +1,14 @@
-import java.io.*;
 import java.util.Scanner;
 
 public class Odd_Even {
    public static void main(String[] args) {
         int n;
         
-        Scanner sc = new Scanner (System.in);
+        try (Scanner sc = new Scanner (System.in)) {
+            System.out.println("Enter a number to find out whether its odd or even: ");
+            n=sc.nextInt();
+        }
         
-        System.out.println("Enter a number to find out whether its odd or even: ");
-        n=sc.nextInt();
-
         if (n%2==0) {
             System.out.println("The number is even.");
         }
